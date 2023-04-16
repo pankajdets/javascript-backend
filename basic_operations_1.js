6d7010afaf5d5957236096ffaaee8f171483111b
Pankaj Kumar Ray <pankaj@appveen.com>
2023-04-16 14:25:58 +0530

    Basic Operations

---
+++ basic_operations_1.js
@@ -0,0 +1,11 @@
+console.log("Hello World!");
+
+console.log(6 && 10); // 10
+
+console.log((6 < 7) && (10 > 7));
+
+console.log(undefined / null); //NaN
+
+console.log(NaN === NaN);
+
+console.log(-4+4);
