9a5189def5851b1fdf87bb48fb8ffa5dc4638149
Pankaj Ray <pankaj.ray@accolite.com>
2023-04-18 17:48:30 +0530

    Examples

--- coercion.js
+++ coercion.js
@@ -5,17 +5,31 @@
 
 // Subtract operator calls abstract operation on both the operand -> ToNumber(argument)
 
-let obj = {
-    // Overriding
-    // Default value is "[object Object]".
-    toString() {
-        return "Let's learn Javascript";
-    },
-    valueOf() {
-        // Default valueOf returns same Object { } -> i.e obj itself.
-        return "Changing default value of valueOf";
-    }
-};
+obj = {
+    x: 10
+}
 
 console.log(obj.toString());
 console.log(obj.valueOf());
+
+console.log(10 - obj);
+
+let obj1 = {
+    // Overriding
+    // Default value is "[object Object]".
+    toString() {
+        return 10;
+    },
+    valueOf() {
+        // Default valueOf returns same Object { } -> i.e obj itself.
+        return 10;
+    }
+};
+
+console.log(obj1.toString());
+console.log(typeof obj1.toString());
+console.log(obj1.valueOf());
+
+console.log(10 - obj1);
+
+console.log(obj + obj1);
